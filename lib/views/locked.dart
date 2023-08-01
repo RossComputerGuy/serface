90e2668a0fd07b373f6e5b9cc53527ad043fa8d6
Tristan Ross <tristan.ross@midstall.com>
2023-08-01 14:27:16 -0700

    feat: added lock screen

--- lib/views/locked.dart
+++ lib/views/locked.dart
@@ -1,8 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:serface/app.dart';
+import 'package:serface/dialogs.dart';
+import 'package:serface/widgets.dart';
 
 class SerfaceLockedView extends StatelessWidget {
   const SerfaceLockedView({ super.key });
 
   @override
-  Widget build(BuildContext context) => const SizedBox();
+  Widget build(BuildContext context) =>
+    Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ...[
+              DigitalClock(
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              Row(
+                children: [
+                  BatteryBar(direction: Axis.horizontal),
+                  NetworkBar(direction: Axis.horizontal),
+                ],
+              ),
+              OutlinedButton(
+                child: Text(
+                  'Unlock',
+                  style: Theme.of(context).textTheme.labelMedium,
+                ),
+                onPressed: () {
+                  showLockDialog(context: context).then((passed) {
+                    if (passed) {
+                      SerfaceApp.of(context).unlock();
+                    }
+                  });
+                }
+              ),
+            ].map(
+              (child) => Padding(
+                padding: const EdgeInsets.all(4),
+                child: child,
+              )
+            ).toList(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
 }
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,2 +1,5 @@
+export 'widgets/battery_bar.dart';
+export 'widgets/clock.dart';
 export 'widgets/keypad.dart';
 export 'widgets/nav.dart';
+export 'widgets/network_bar.dart';
